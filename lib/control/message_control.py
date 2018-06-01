e2cfad1fd77300376c0f257ad216870ff8277a05
changsuwei <F74036255@mail.ncku.edu.tw>
2018-06-01 20:42:58 +0800

    debug

--- lib/control/message_control.py
+++ lib/control/message_control.py
@@ -9,7 +9,7 @@ def message_control(messaging_event, sender_id):
         message_text = messaging_event["message"][
             "text"]  # the message's text
         print(sender_id)  # test
-        flag = get_flag(sender_id)
+        # flag = get_flag(sender_id)
         if message_text == u'hello':
             json_message(
                 sender_id, "hello")

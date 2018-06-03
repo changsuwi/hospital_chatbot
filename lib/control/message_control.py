f3a61cd92e47418b5b10d461187c46b7018d15f1
changsuwei <F74036255@mail.ncku.edu.tw>
2018-06-03 14:56:33 +0800

    debug

--- lib/control/message_control.py
+++ lib/control/message_control.py
@@ -14,7 +14,12 @@ def message_control(messaging_event, sender_id):
             json_message(sender_id, "您好，我是醫護助理聊天機器人，請問需要什麼服務呢?")
             json_mainbutton(sender_id)
         else:
-            if get_flag(sender_id) == 1:
+            if "quick_reply" in messaging_event["message"]:
+                if messaging_event["message"]["quick_reply"]["payload"] == "main_button1":
+                    upload_flag(1, sender_id)
+                    json_message(sender_id, "您的身體哪裡不舒服?")
+
+            elif get_flag(sender_id) == 1:
                 json_message(
                     sender_id, "目前正在搜查比對資料庫 請稍後")
 

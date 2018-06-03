a1ae70038dac85ca1f4d1a456ead9ef8241fd2c0
changsuwei <F74036255@mail.ncku.edu.tw>
2018-06-03 14:48:42 +0800

    debug

--- lib/control/message_control.py
+++ lib/control/message_control.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-from ..json_fb import typingon_json, json_message
-from db import get_flag
+from ..json_fb import typingon_json, json_message, json_mainbutton
+from db import get_flag, upload_flag
 
 
 def message_control(messaging_event, sender_id):
@@ -9,14 +9,15 @@ def message_control(messaging_event, sender_id):
         message_text = messaging_event["message"][
             "text"]  # the message's text
         print(sender_id)  # test
-        flag = get_flag(sender_id)
-        if message_text == u'hello':
-            json_message(
-                sender_id, "hello")
-        elif flag == 1:
-            json_message(
-                sender_id, "目前正在搜查比對資料庫 請稍後")
+        if message_text == u'hello' or u'Hello':
+            upload_flag(0, sender_id)
+            json_message(sender_id, "您好，我是醫護助理聊天機器人，請問需要什麼服務呢?")
+            json_mainbutton(sender_id)
+        else:
+            if get_flag(sender_id) == 1:
+                json_message(
+                    sender_id, "目前正在搜查比對資料庫 請稍後")
 
-            '''
-            接冠文的code
-            '''
+                '''
+                接冠文的code
+                '''

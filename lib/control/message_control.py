34f87c2eae355402e8e2a58cbb5809bc5aa5d25e
changsuwei <F74036255@mail.ncku.edu.tw>
2018-06-03 14:42:04 +0800

    demo_1

--- lib/control/message_control.py
+++ lib/control/message_control.py
@@ -9,10 +9,13 @@ def message_control(messaging_event, sender_id):
         message_text = messaging_event["message"][
             "text"]  # the message's text
         print(sender_id)  # test
-        # flag = get_flag(sender_id)
+        flag = get_flag(sender_id)
         if message_text == u'hello':
             json_message(
                 sender_id, "hello")
+        elif flag == 1:
+            json_message(
+                sender_id, "目前正在搜查比對資料庫 請稍後")
 
             '''
             接冠文的code
